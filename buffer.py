47db3db56459af3a0784503b2924f49eca67d277
unknown <Humberto@.(none)>
2012-04-14 22:12:04 -0300

    buffer logic

---
+++ buffer.py
@@ -0,0 +1,41 @@
+from Tkinter import *
+import text
+
+class Buffer(object):
+    """Manage file content in the editor
+    """
+
+    def __init__(self, textarea, filename=None):
+        self.filename = filename
+        self.textarea = textarea
+        self.md5 = None
+
+    def get_contents(self):
+        return self.textarea.get(1.0, END)
+
+    def append(self, text):
+        """
+        Insert text in the end of the buffer
+        """
+        self.textarea.insert(END, text)
+
+    def insert(self, text):
+        """
+        Insert text at current position
+        """
+        self.textarea.insert(INSERT, text)
+
+    def save(self):
+        file = open(self.filename, 'w+')
+        file.write(self.get_contents())
+        file.close
+
+    def load_file(self):
+        file = open(self.filename, 'r')
+        contents = file.read()
+        file.close()
+        self.textarea.delete(1.0, END)
+        self.textarea.insert(1.0, contents)
+        self.md5 = text.md5sum(contents)
+
+    contents = property(get_contents)
--- mainwindow.py
+++ mainwindow.py
@@ -2,6 +2,7 @@ from Tkinter import Tk, RIGHT, LEFT, Y, Text, END, Frame, Label, Scrollbar, BOTH
 import text
 import tkFileDialog
 import tkMessageBox
+from buffer import Buffer
 
 class MainWindow(Frame):
     """
@@ -11,16 +12,15 @@ class MainWindow(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.set_title('pyeditor')
-        self.master.geometry('800x600')
-        self.open_files = []
+        self.master.geometry('640x480')
+        self.buffers = []
+        self.current_buffer = None
         self.create_widgets()
         self.set_keyboard_shortcuts()
-
 
     def create_widgets(self):
         self.master.protocol('WM_DELETE_WINDOW', self.quit)
         self.create_text_area()
-
 
     def create_text_area(self):
         self.text_area = Text(self.master, height=20, width=72)
@@ -30,68 +30,56 @@ class MainWindow(Frame):
         self.text_area.pack(expand=1, fill=BOTH)
         s.config(command=self.text_area.yview)
         self.text_area.config(yscrollcommand=s.set)
-
+        self.buffers.append(Buffer(self.text_area))
+        self.current_buffer = self.buffers[0]
 
     def get_contents(self):
-        return self.text_area.get(1.0, END)
+        return self.current_buffer.contents.strip()
 
-
-    def save_file(self, path):
-        file = open(path, 'w+')
-        file.write(self.get_contents())
-        file.close()
-
+    def save_file(self, event=None, exiting=False):
+        if self.current_buffer.filename is None and len(self.get_contents()) > 0:
+            filename = self.show_save_dialog()
+            self.current_buffer.filename = filename
+            self.current_buffer.save()
+            return
+        if exiting:
+            if self.show_question('Save file %s?' % self.current_buffer.filename):
+                self.current_buffer.save()
+            return
+        self.current_buffer.save()
 
     def open_file(self, path):
         self.check_files_saved()
-        file = open(path, 'r')
-        contents = file.read()
-        self.text_area.delete(1.0, END)
-        self.text_area.insert(1.0, contents)
-        file.close()
-        self.open_files.append((path, text.md5sum(contents)))
+        self.current_buffer.filename = path
+        self.current_buffer.load_file()
         self.set_title(path)
 
-
-    def show_save_dialog(self, event=None, path=None):
-        if not path is None:
-            answer = tkMessageBox.askyesno('Save', 'Save file %s ?' % path)
-            if answer:
-                self.save_file(path)
-        else:
-            path = tkFileDialog.asksaveasfilename(title='Save file as')
-            if len(path) > 0:
-                self.save_file(path)
-
+    def show_save_dialog(self):
+        path = tkFileDialog.asksaveasfilename(title='Save file as')
+        return path
 
     def check_files_saved(self):
-        contents = self.get_contents()
-        if len(self.open_files) > 0:
-            path, md5sum = self.open_files[0]
-            if md5sum != text.md5sum(contents):
-                self.show_save_dialog(path=path)
-        elif len(contents.strip()) > 0:
-            self.show_save_dialog()
-
+        self.save_file(exiting=True)
 
     def quit(self):
         self.check_files_saved()
         self.master.destroy()
 
-
     def show_open_dialog(self, event=None):
         path = tkFileDialog.askopenfilename(title='Open file')
-        self.open_file(path.strip())
-
+        if len(path) > 0:
+            self.current_buffer.filename = path
+            self.current_buffer.load_file()
 
     def set_keyboard_shortcuts(self):
         self.master.bind('<Control-o>', self.show_open_dialog)
-
+        self.master.bind('<Control-s>', self.save_file)
 
     def set_title(self, title):
         self.master.title(title)
 
-
+    def show_question(self, msg):
+        return tkMessageBox.askyesno('pyeditor', msg)
 
 
 def run():
